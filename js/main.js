77dd56a32bcd4d425b6cfb077b375a1c9ba4d022
shusukeO <sokihara924@gmail.com>
2021-03-20 17:19:43 +0900

    maxzoomの変更

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ window.onload = function () {
   L.tileLayer(
     'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
     {
-      maxZoom: 18,
+      maxZoom: 21,
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
